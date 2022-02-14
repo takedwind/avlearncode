91c32c66e5edab36142f70396705dbd415a71d3c
qinyong <takedwind@gmail.com>
2022-02-15 00:39:27 +0800

    update

---
+++ Android/20.H265编码原理与码流分析/H265SPS/cmake-build-debug/CMakeFiles/H265SPS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/H265SPS.dir/main.cpp.obj"
+  "H265SPS.exe"
+  "H265SPS.exe.manifest"
+  "H265SPS.pdb"
+  "libH265SPS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/H265SPS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
